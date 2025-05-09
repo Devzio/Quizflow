a403c28253e245de575a7755d30d039be95ca739
Sheng Lee <jason83925@gmail.com>
2025-05-09 20:18:26 +1000

    chore: update constants

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,187 +1,158 @@
 
 
 export const initialNodes = [
-  // {
-  //   id: '1',
-  //   type: 'start',
-  //   data: {
-  //     label: (
-  //       <>
-  //         Node <strong>A</strong>
-  //       </>
-  //     ),
-  //   },
-  //   position: { x: 0, y: 0 },
-  // },
-  // {
-  //   id: '2',
-  //   type: 'text',
-  //   data: {
-  //     label: (
-  //       <>
-  //         Question <strong>1</strong>
-  //       </>
-  //     ),
-  //   },
-  //   position: { x: 0, y: 100 },
-  // },
-  // {
-  //   id: '3',
-  //   type: 'text',
-  //   data: {
-  //     label: (
-  //       <>
-  //         Question 2A
-  //       </>
-  //     ),
-  //   },
-  //   position: { x: -150, y: 200 },
-  //   // style: {
-  //   //   background: '#D6D5E6',
-  //   //   color: '#333',
-  //   //   width: 180,
-  //   // },
-  // },
-  // {
-  //   id: '4',
-  //   type: 'text',
-  //   data: {
-  //     label: (
-  //       <>
-  //         Question 2B
-  //       </>
-  //     ),
-  //   },
-  //   position: { x: 150, y: 200 },
-  // },
-  // {
-  //   id: '5',
-  //   type: 'text',
-  //   data: {
-  //     label: (
-  //       <>
-  //         Question 3A
-  //       </>
-  //     ),
-  //   },
-  //   position: { x: -300, y: 300 },
-  // },
-  // {
-  //   id: '6',
-  //   type: 'text',
-  //   data: {
-  //     label: (
-  //       <>
-  //         Question 3B
-  //       </>
-  //     ),
-  //   },
-  //   position: { x: 0, y: 300 },
-  // },
-  // {
-  //   id: '7',
-  //   type: 'text',
-  //   data: {
-  //     label: (
-  //       <>
-  //         Question 3C
-  //       </>
-  //     ),
-  //   },
-  //   position: { x: 300, y: 300 },
-  // },
-  // {
-  //   id: '8',
-  //   type: 'end',
-  //   data: {
-  //     label: (
-  //       <>
-  //         Question 3C
-  //       </>
-  //     ),
-  //   },
-  //   position: { x: 0, y: 400 },
-  // },
-];
+//   {
+//     id: '1',
+//     type: 'start',
+//     data: {
+//       label: "A",
+//       choiceType: "type_name"
+//     },
+//     position: { x: 0, y: 0 },
+//   },
+//   {
+//     id: '2',
+//     type: 'text',
+//     data: {
+//       label: "Question 1",
+//     },
+//     position: { x: 0, y: 100 },
+//   },
+//   {
+//     id: '3',
+//     type: 'text',
+//     data: {
+//       label: "Question 2A"
+//     },
+//     position: { x: -150, y: 200 },
+//   },
+//   {
+//     id: '4',
+//     type: 'text',
+//     data: {
+//       label: "Question 2B"
+//     },
+//     position: { x: 150, y: 200 },
+//   },
+//   {
+//     id: '5',
+//     type: 'text',
+//     data: {
+//       label: (
+//         <>
+//           Question 3A
+//         </>
+//       ),
+//     },
+//     position: { x: -300, y: 300 },
+//   },
+//   {
+//     id: '6',
+//     type: 'text',
+//     data: {
+//       label: "Question 3B"
+//     },
+//     position: { x: 0, y: 300 },
+//   },
+//   {
+//     id: '7',
+//     type: 'text',
+//     data: {
+//       label: "Question 3C"
+//     },
+//     position: { x: 300, y: 300 },
+//   },
+//   {
+//     id: '8',
+//     type: 'end',
+//     data: {
+//       label: "Question 3C"
+//     },
+//     position: { x: 0, y: 400 },
+//   },
+// ];
 
-export const initialEdges = [
-  // {
-  //   id: 'e1-2',
-  //   type: 'edgedelete',
-  //   animated: true,
-  //   source: '1',
-  //   target: '2',
-  //   label: 'Start',
-  //   reconnectable: true,
-  // },
-  // {
-  //   id: 'e2-3',
-  //   type: 'default',
-  //   animated: true,
-  //   source: '2',
-  //   target: '3',
-  //   label: 'No',
-  //   reconnectable: true,
-  // },
-  // {
-  //   id: 'e2-4',
-  //   type: 'default',
-  //   animated: true,
-  //   source: '2',
-  //   target: '4',
-  //   label: 'Yes',
-  // },
-  // {
-  //   id: 'e3-5',
-  //   type: 'default',
-  //   animated: true,
-  //   source: '3',
-  //   target: '5',
-  //   label: 'Yes',
-  // },
-  // {
-  //   id: 'e3-6',
-  //   type: 'default',
-  //   animated: true,
-  //   source: '3',
-  //   target: '6',
-  //   label: 'No',
-  // },
-  // {
-  //   id: 'e4-6',
-  //   type: 'default',
-  //   animated: true,
-  //   source: '4',
-  //   target: '6',
-  //   label: 'No',
-  // },
-  // {
-  //   id: 'e4-7',
-  //   type: 'default',
-  //   animated: true,
-  //   source: '4',
-  //   target: '7',
-  //   label: 'Yes',
-  // },
-  // {
-  //   id: 'e5-8',
-  //   type: 'edgedelete',
-  //   animated: true,
-  //   source: '5',
-  //   target: '8',
-  // },
-  // {
-  //   id: 'e6-8',
-  //   type: 'edgedelete',
-  //   animated: true,
-  //   source: '6',
-  //   target: '8',
-  // },
-  // {
-  //   id: 'e7-8',
-  //   type: 'edgedelete',
-  //   animated: true,
-  //   source: '7',
-  //   target: '8',
-  // },
+// export const initialEdges = [
+//   {
+//     id: 'e1-2',
+//     type: 'edgedelete',
+//     animated: true,
+//     source: '1',
+//     target: '2',
+//     label: 'Start',
+//     reconnectable: true,
+//     data: {
+//       choiceType: "type_name"
+//     }
+//   },
+//   {
+//     id: 'e2-3',
+//     type: 'default',
+//     animated: true,
+//     source: '2',
+//     target: '3',
+//     label: 'No',
+//     reconnectable: true,
+//   },
+//   {
+//     id: 'e2-4',
+//     type: 'default',
+//     animated: true,
+//     source: '2',
+//     target: '4',
+//     label: 'Yes',
+//   },
+//   {
+//     id: 'e3-5',
+//     type: 'default',
+//     animated: true,
+//     source: '3',
+//     target: '5',
+//     label: 'Yes',
+//   },
+//   {
+//     id: 'e3-6',
+//     type: 'default',
+//     animated: true,
+//     source: '3',
+//     target: '6',
+//     label: 'No',
+//   },
+//   {
+//     id: 'e4-6',
+//     type: 'default',
+//     animated: true,
+//     source: '4',
+//     target: '6',
+//     label: 'No',
+//   },
+//   {
+//     id: 'e4-7',
+//     type: 'default',
+//     animated: true,
+//     source: '4',
+//     target: '7',
+//     label: 'Yes',
+//   },
+//   {
+//     id: 'e5-8',
+//     type: 'edgedelete',
+//     animated: true,
+//     source: '5',
+//     target: '8',
+//   },
+//   {
+//     id: 'e6-8',
+//     type: 'edgedelete',
+//     animated: true,
+//     source: '6',
+//     target: '8',
+//   },
+//   {
+//     id: 'e7-8',
+//     type: 'edgedelete',
+//     animated: true,
+//     source: '7',
+//     target: '8',
+//   },
 ];
